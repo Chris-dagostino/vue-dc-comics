ad98d365ebb6b008729af141aa62825991478df3
Chris-dagostino <dagostino.christian94@gmail.com>
2022-02-21 15:42:17 +0100

    start

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <MyHeader></MyHeader>
+    <MyMain></MyMain>
+    <MyFooter></MyFooter>
+    
+    
+  </div>
+</template>
+
+<script>
+import MyMain from './components/MyMain.vue';
+import MyFooter from './components/MyFooter.vue';
+import MyHeader from './components/MyHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MyHeader,
+    MyMain,
+    MyFooter
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+ 
+}
+</style>
